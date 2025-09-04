486b8a790a68a8663262e464f9a4b6b2a8b99691
hontas <lundin.pontus@gmail.com>
2025-09-04 09:51:28 +0200

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -16,14 +16,9 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: 'vite.svg',
+            sizes: '256x256',
+            type: 'image/svg',
           },
         ],
       },
